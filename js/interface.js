2755a4a6cf36cef0b4569b22ac26bc905324a30d
Brandon Walsh <walshb@wlu.edu>
2016-04-18 12:22:26 -0400

    major tweaks for responsiveness

--- js/interface.js
+++ js/interface.js
@@ -19,7 +19,7 @@ function reveal_visualize(){
 function generate_css_link(){
     var cssLink = document.createElement("link"); 
     // cssLink.href = "css/custom.css";
-    cssLink.href = "http://mdata1.ad.wlu.edu/ShenandoahExplorer/css/custom.css";
+    cssLink.href = "https://fonts.googleapis.com/css?family=Habibi";
     cssLink.rel = "stylesheet"; 
     cssLink.type = "text/css";
     return cssLink;
@@ -27,12 +27,9 @@ function generate_css_link(){
 
 function add_css_to_iframe(){
     frames['search-frame'].document.head.appendChild(generate_css_link());
-    frames['browse-frame'].document.head.appendChild(generate_css_link());
-    frames['visualize-frame'].document.head.appendChild(generate_css_link());
-    console.log('hello');
 };
 
 function prep_page(){
-    add_css_to_iframe();    
+    // add_css_to_iframe();    
     reveal_search();
 };
